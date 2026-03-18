39d8e26b4510d02e70f2138667d6a6d5b6b70cb8
soruh <mail@soruh.de>
2026-03-18 18:02:50 +0100

    silence warnings for useless into

--- einstellung_derive/src/derive_config/generator.rs
+++ einstellung_derive/src/derive_config/generator.rs
@@ -167,6 +167,7 @@ fn generate_partial_impl(model: &TransformedStruct) -> TokenStream {
                 if let Err(e) = (#validate_func)(&#ident) {
                     return Err(#einstellung::ConfigError::Validation {
                         field: #einstellung::FieldPath::new(#complete_str, #ident_str),
+                        #[allow(clippy::useless_conversion)]
                         reason: e.into(),
                     });
                 }
